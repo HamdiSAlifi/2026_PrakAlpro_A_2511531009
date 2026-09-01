56cae19e3f28bca6c8896403e9cd34ac525722f2
HamdiSAlifi <2511531009_hamdi@student.unand.ac.id>
2026-09-01 09:42:59 +0700

    Pekan 1 Praktikum Alpro

---
+++ Pekan1/program1.py
@@ -0,0 +1,14 @@
+# Program Pertama
+
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+
+print("Hello World")
+print()
+print("Program ini menghasilan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini " + "dibuat an. " + "Hamdi Sidqi Alifi", "2511531009")
