6706d3796b98c852209bdcfadde5dbd610769e50
HamdiSAlifi <2511531009_hamdi@student.unand.ac.id>
2026-09-08 08:55:22 +0700

    Pekan 2 Praktikum Algoritma dan Pemograman

---
+++ Pekan2_2511531009/numerik1_2511531009.py
@@ -0,0 +1,11 @@
+# Create a variable with integer value
+a_1009 = 100
+print("the type of variable having value ", a_1009, "is ", type(a_1009))
+
+#
+b_1009 = 10.2345
+print("the type of varialbe having value ", b_1009, "is ", type(b_1009))
+
+#
+c_1009 = 3j
+print("the type of varialbe having value ", c_1009, "is ", type(c_1009))
