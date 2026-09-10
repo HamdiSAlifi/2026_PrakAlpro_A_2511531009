924e0ece4b4987defca820c297b8ded4146b69de
HamdiSAlifi <2511531009_hamdi@student.unand.ac.id>
2026-09-11 06:00:00 +0700

    Laporan Praktikum Alpro Pekan 2

--- Pekan2_2511531009/Input_2511531009 copy.py
+++
@@ -1,6 +0,0 @@
-panjang_1009 = input("Masukkan nilai panjang  : ")
-lebar_1009 = input("Masukkan nilai lebar    : ")
-
-luas_1009 = int(panjang_1009) * int(lebar_1009)
-
-print("Luas : ", luas_1009)
--- Pekan2_2511531009/Konstanta_2511531009.py
+++ Pekan2_2511531009/Konstanta_2511531009.py
@@ -1,8 +1,8 @@
 from typing import final
-PI_xc: final = 3.14
+PI_1009: final = 3.14
 
-print("pi: %f" % (PI_xc))
-jari_xc = float(input("masukkan nilai jari-jari : "))
-luas_xc = PI_xc * jari_xc * jari_xc
+print("pi: %f" % (PI_1009))
+jari_1009 = float(input("masukkan nilai jari-jari : "))
+luas_1009 = PI_1009 * jari_1009 * jari_1009
 
-print("Luas lingkaran dengan jari jari %f adalah %f" % (jari_xc, luas_xc))
+print("Luas lingkaran dengan jari jari %f adalah %f" % (jari_1009, luas_1009))
